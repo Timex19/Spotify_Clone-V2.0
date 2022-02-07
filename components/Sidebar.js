dae0910363b1ea7106562b94219a84b26c770621
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2022-02-07 01:22:01 +0100

    [Fixed]: refactored tracks

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -11,7 +11,7 @@ import React from 'react';
 
 function Sidebar() {
     return (
-        <section className='fixed top-0 z-20 flex flex-col p-4 items-center 
+        <section className='fixed top-0 z-40 flex flex-col p-4 items-center 
         bg-black w-[90px] h-screen space-y-8'>
             <Image src="https://rb.gy/xkacau" 
             width={56} 
